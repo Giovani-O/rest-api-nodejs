8dbdbcd7800832b732f2b1a1e9be4989d3201ac7
Giovani <giovani98@proton.me>
2024-10-02 15:44:50 -0300

    feat: Setting up middlewares

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,42 +2,62 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
 import crypto from 'node:crypto'
+import { checkIfSessionIdExists } from '../middlewares/check-if-session-id-exists'
 
 // plugin
 export async function transactionRoutes(app: FastifyInstance) {
   // GET endpoints
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  // O get abaixo tem um middleware checkIfSessionIdExists
+  app.get('/', { preHandler: [checkIfSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
     return {
       transactions,
     }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkIfSessionIdExists] }, async (request) => {
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
+    const { sessionId } = request.cookies
+
     // Busca por id, semelhante ao FirstOrDefault do .NET
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
 
     return { transaction }
   })
 
   // GET endpoint, obtém a soma das transações
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
+  app.get(
+    '/summary',
+    { preHandler: [checkIfSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-    return summary
-  })
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+
+      return summary
+    },
+  )
 
   // POST endpoint, tipado com zod
-  app.post('/', async (request, response) => {
+  app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
@@ -49,13 +69,27 @@ export async function transactionRoutes(app: FastifyInstance) {
       request.body,
     )
 
+    // Busca sessionId dos cookies
+    let sessionId = request.cookies.sessionId
+
+    // Cria sessionId e salva nos cookies
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 dias
+      })
+    }
+
     // Salva no banco de dados, semelhante a repository/unit of work
     await knex('transactions').insert({
       id: crypto.randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
-    return response.status(201).send()
+    return reply.status(201).send()
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { env } from './env'
 import { transactionRoutes } from './routes/transactions'
@@ -8,6 +9,15 @@ const app = fastify()
 
 // Aqui são definidos os plugins
 // É bem semelhante ao builder.Services.Add... que é usado no .NET
+// A ordem dos registros importa
+
+app.register(cookie)
+
+// Middleware global
+app.addHook('preHandler', async (request) => {
+  console.log(`${request.method} - ${request.url}`)
+})
+
 app.register(transactionRoutes, {
   prefix: 'transactions',
 })
